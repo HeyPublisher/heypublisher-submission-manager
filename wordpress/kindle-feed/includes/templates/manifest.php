aac09687765005afb0268623ee233b8bf13cf23b
rluck <rluck@f4a7a2ea-807d-4039-9a8e-9222a151f367>
2011-11-21 01:51:12 +0000

    tweak

--- wordpress/kindle-feed/includes/templates/manifest.php
+++ wordpress/kindle-feed/includes/templates/manifest.php
@@ -29,7 +29,7 @@ echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; ?>
   foreach ($categories as $slug=>$link) { 
 ?>
   <item>
-    <link><?php printf("%skindle_section.xml", $link); ?></link>
+    <link><?php printf("%sfee/kindle_section", $link); ?></link>
   </item>
 <?php 
   }; 
