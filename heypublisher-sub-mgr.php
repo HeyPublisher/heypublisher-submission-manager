11beb277d9871d1f2351aa410c0561d10e2e1ef5
Richard Luck <richard@loudlever.com>
2017-12-20 19:55:41 -0800

    upping version ot 2.6.3

--- heypublisher-sub-mgr.php
+++ heypublisher-sub-mgr.php
@@ -5,7 +5,7 @@ Plugin URI: https://www.heypublisher.com
 Description: HeyPublisher is a better way of managing unsolicited submissions directly within WordPress.
 Author: HeyPublisher
 Author URI: https://www.heypublisher.com
-Version: 2.6.2
+Version: 2.6.3
 
   Copyright 2010-2014 Loudlever, Inc. (wordpress@loudlever.com)
   Copyright 2014-2017 Richard Luck (https://github.com/aguywithanidea/)
@@ -71,16 +71,17 @@ define('HEY_DIR', dirname(plugin_basename(__FILE__)));
   2.6.0 => 67
   2.6.1 => 68
   2.6.2 => 69
+  2.6.3 => 70
 ---------------------------------------------------------------------------------
 */
 
 // Configs specific to the plugin
 // Build Number (must be a integer)
 define('HEY_BASE_URL', get_option('siteurl').'/wp-content/plugins/'.HEY_DIR.'/');
-define("HEYPUB_PLUGIN_BUILD_DATE", "2017-12-19");
+define("HEYPUB_PLUGIN_BUILD_DATE", "2017-12-20");
 // Version Number (can be text)
-define("HEYPUB_PLUGIN_BUILD_NUMBER", "69");  // This controls whether or not we get upgrade prompt
-define("HEYPUB_PLUGIN_VERSION", "2.6.2");
+define("HEYPUB_PLUGIN_BUILD_NUMBER", "70");  // This controls whether or not we get upgrade prompt
+define("HEYPUB_PLUGIN_VERSION", "2.6.3");
 
 # Base domain
 $domain = 'https://www.heypublisher.com';
