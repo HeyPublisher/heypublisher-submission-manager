dc49697de5ecada3ac98764c2317c10b286426f5
Richard Luck <richard@dimax.com>
2020-03-08 16:24:12 -0700

    fixing issue where multiple calls need to be made to this function.  This allows repeated calls without hitting the API multiple times

--- include/classes/HeyPublisher/API/Publisher.class.php
+++ include/classes/HeyPublisher/API/Publisher.class.php
@@ -9,6 +9,7 @@ if(preg_match('#' . basename(__FILE__) . '#', $_SERVER['PHP_SELF'])) { die('HeyP
 
 require_once(HEYPUB_PLUGIN_FULLPATH . '/include/classes/HeyPublisher/API.class.php');
 class Publisher extends \HeyPublisher\API {
+  var $publisher = false;
 
   public function __construct() {
   	parent::__construct();
@@ -21,9 +22,14 @@ class Publisher extends \HeyPublisher\API {
   // Get the Publisher info for display in the Plugin Options
   public function get_publisher_info(){
     $path = sprintf('publishers/%s',$this->poid);
+    if ($this->publisher) {
+      // we've already fetched this data - just return from memory
+      return $this->publisher;
+    }
     $result = $this->get($path);
     $this->logger->debug(sprintf("get_publisher_info():\n\tResults: %s",print_r($result,1)));
     if (key_exists('publishers',$result)) {
+      $this->publisher = $result['publishers'][0];
       return $result['publishers'][0];
     }
     $this->error = 'Error finding publisher info on HeyPublisher.com';
