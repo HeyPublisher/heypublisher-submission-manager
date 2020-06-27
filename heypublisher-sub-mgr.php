1bf7f12ccaa0dcf0eb72e5ea1d2cd4abb99e9c19
Richard Luck <richard@dimax.com>
2020-06-27 13:42:56 -0700

    missed updating the version in the main file :(

--- heypublisher-sub-mgr.php
+++ heypublisher-sub-mgr.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/HeyPublisher/heypublisher-submission-manager
 Description: HeyPublisher is a better way of managing unsolicited submissions directly within WordPress.
 Author: HeyPublisher
 Author URI: https://www.heypublisher.com
-Version: 2.9.1
+Version: 3.0.1
 Requires at least: 4.0
 
 
@@ -82,6 +82,7 @@ define('HEY_DIR', dirname(plugin_basename(__FILE__)));
   2.8.3 => 75
   2.9.0 => 76
   3.0.0 => 80
+  3.0.1 => 81
 
 ---------------------------------------------------------------------------------
 */
@@ -91,7 +92,7 @@ define('HEY_DIR', dirname(plugin_basename(__FILE__)));
 define('HEY_BASE_URL', get_option('siteurl').'/wp-content/plugins/'.HEY_DIR.'/');
 define("HEYPUB_PLUGIN_BUILD_DATE", "2020-06-27");
 // Version Number (can be text)
-define("HEYPUB_PLUGIN_BUILD_NUMBER", "80");  // This controls whether or not we get upgrade prompt
+define("HEYPUB_PLUGIN_BUILD_NUMBER", "81");  // This controls whether or not we get upgrade prompt
 define("HEYPUB_PLUGIN_VERSION", "3.0.1");
 define("HEYPUB_PLUGIN_TESTED", "5.3.0");
 
